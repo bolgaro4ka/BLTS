4fd4b8eaf8ca949653ceeefb438aefde9c0eaebb
Nikita <bolgaro4ka.github@gmail.com>
2024-06-02 18:31:39 +0300

    Update s.py

--- s.py
+++ s.py
@@ -24,4 +24,5 @@ os.system("python manage.py makemigrations tests")
 os.system("python manage.py migrate")
 
 print(Fore.GREEN+'Сервер запущен! Откройте в браузере http://{}:{}. Нажмите Ctrl+C для выхода. Good job!'.format(args.ip, args.port))
+print(Fore.YELLOW+'Внимание! Чтобы создать суперпользователя для админ панели выполните команду: python manage.py createsuperuser')
 os.system(f"python manage.py runserver {args.ip}:{args.port}")
